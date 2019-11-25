7e1b5bb5ae8fa12bee28039a026f449f2a4b0e01
chinjsg <dragvern@hotmail.com>
2019-11-24 19:19:59 -0700

    updated variables

--- src/enemies/Enemy.java
+++ src/enemies/Enemy.java
@@ -3,19 +3,26 @@ package enemies;
 public abstract class Enemy {
 	protected int health;
 	protected int speed;
+	protected int goldReward;
 	//protected image image;
 	//protected Title tile;
-	public Enemy(int health, int speed) {
+	
+	public Enemy(int health, int speed, int goldReward) {
 		this.health = health;
 		this.speed = speed;
+		this.goldReward = goldReward;
 	}
 	
 	public int getHealth() {
 		return health;
 	}
+	
 	public int getSpeed() {
 		return speed;
 	}
 	
+	public int getGoldReward() {
+		return goldReward;
+	}
 
 }
--- src/towers/Tower.java
+++ src/towers/Tower.java
@@ -4,21 +4,30 @@ public class Tower {
 	protected int attackPower;
 	protected int rateOfFire;
 	protected boolean areaDamage;	// single target - aoe
+	protected int cost;
 	//protected image image;
 	//protected Title tile;
-	public Tower(int attackPower, int rateOfFire, boolean areaDamage) {
+	
+	public Tower(int attackPower, int rateOfFire, boolean areaDamage, int cost) {
 		this.attackPower = attackPower;
 		this.rateOfFire = rateOfFire;
 		this.areaDamage = areaDamage;
+		this.cost = cost;
 	}
 	
 	public int getAttackPower() {
 		return attackPower;
 	}
+	
 	public int getRateOfFire() {
 		return rateOfFire;
 	}
+	
 	public boolean getAreaDamage() {
 		return areaDamage;
+	}
+	
+	public int getCost() {
+		return cost;
 	}
 }
