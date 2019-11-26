37cfb737fa65877b5e5a32db4621bb2799a20b31
abhisheksharma1042 <abhisheksharma1042@users.noreply.github.com>
2019-11-26 14:31:42 -0700

    milestone 1 ready

--- src/game/TowersOfBrimstoneView.java
+++ src/game/TowersOfBrimstoneView.java
@@ -1,5 +1,6 @@
 package game;
 
+
 import java.util.ArrayList;
 
 import experimenting.ImageButton;
@@ -14,6 +15,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ButtonBase;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
@@ -28,35 +30,38 @@ import javafx.stage.Stage;
 import towers.Tower;
 
 public class TowersOfBrimstoneView extends Application {
-	
-	ImageButton stone;
-	ImageButton fire;
-	ImageButton ice;
-	ImageButton heavy;
-	ImageButton lightning;
-	ImageButton magic;
-	ImageButton slowdown;
-	ImageButton damageboost;
-	
-	private TowersOfBrimstoneModel model;
+
+    ImageButton stone;
+    ImageButton fire;
+    ImageButton ice;
+    ImageButton heavy;
+    ImageButton lightning;
+    ImageButton magic;
+    ImageButton slowdown;
+    ImageButton damageboost;
+
+    private TowersOfBrimstoneModel model;
     private TowersOfBrimstoneController controller;
     private int money;
     private AnchorPane base;
     private boolean togglePlacement;
-	private Tower selectedTower;
-	private int towerType;
+    private Tower selectedTower;
+    private int towerType;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 	base = new AnchorPane();
+	
 	BorderPane root = new BorderPane();
 	Canvas canvas = new Canvas(1400, 1000);
 	Canvas canvas2 = new Canvas(1400, 1000);
 	GraphicsContext gc2 = canvas2.getGraphicsContext2D();
 	GraphicsContext gc = canvas.getGraphicsContext2D();
-	//gc.drawImage(new Image("easyMap.png",1400,1000, false, false),0, 0);
+	 gc.drawImage(new Image("easyMap.png",1400,1000, false, false),0, 0);
 	GraphicsContext valueLayer = canvas.getGraphicsContext2D();
 	GraphicsContext towerMenuLayer = canvas.getGraphicsContext2D();
+	String str = "Gold: " + Integer.toString(money);
+	Label gold = new Label(str);
 
 	model = new TowersOfBrimstoneModel();
 	controller = new TowersOfBrimstoneController(model);
@@ -70,72 +75,74 @@ public class TowersOfBrimstoneView extends Application {
 	towerType = 1; // temp
 	selectedTower = controller.getTowerType(1); // temp
 
-	gc.fillText("Gold: " + Integer.toString(money), 100, 100);
-	
+//	gc.fillText("Gold: " + Integer.toString(money), 100, 100);
+
 	root.getChildren().add(canvas);
 	root.getChildren().add(canvas2);
 	base.getChildren().add(root);
+	base.getChildren().add(gold);
+	base.setTopAnchor(gold, 200.00);
+	base.setLeftAnchor(gold, 200.00);
 	setUpTowerMenu();
 	Scene scene = new Scene(base, 1400, 1000);
 	canvas2.setOnMouseClicked((event) -> {
 	    int xPos = (int) event.getX();
 	    int yPos = (int) event.getY();
-	    int row = yPos/50;
-		int col = xPos/50;
-		
-		System.out.println("ROW " + row + " COL " + col);
-	    
-	    
-	    
-		// Check tower on this spot
-		controller.checkTower(row, col);
-		
-		// Used to stop placement on Right Click
-		if (event.getButton() == MouseButton.SECONDARY && togglePlacement == true) {
-			togglePlacement = false;
-			System.out.println("Tower Placement disabled!");
+	    int row = yPos / 50;
+	    int col = xPos / 50;
+
+	    System.out.println("ROW " + row + " COL " + col);
+
+	    // Check tower on this spot
+	    controller.checkTower(row, col);
+
+	    // Used to stop placement on Right Click
+	    if (event.getButton() == MouseButton.SECONDARY && togglePlacement == true) {
+		togglePlacement = false;
+		System.out.println("Tower Placement disabled!");
+	    }
+
+	    // Tower Placement logic
+	    int temp = 15; // depending on where the store menu starts graphically
+	    if (togglePlacement == true && row < temp) {
+		boolean isPlaced = controller.placeTower(row, col, selectedTower);
+		if (isPlaced) {
+		    money = model.getGold();
+		    System.out.println("Tower has been placed!");
+		} else {
+		    System.out.println("Cannot place on this spot or insufficient funds");
 		}
-		
-		// Tower Placement logic
-		int temp = 15; //depending on where the store menu starts graphically
-		if (togglePlacement == true && row < temp) {
-			boolean isPlaced = controller.placeTower(row, col, selectedTower);
-			if (isPlaced) {
-				money = model.getGold();
-				System.out.println("Tower has been placed!");
-			} else {
-				System.out.println("Cannot place on this spot or insufficient funds");
-			}
-		}
+	    }
 	});
-	
-	
+
 	primaryStage.setScene(scene);
 	primaryStage.show();
 
 	new AnimationTimer() {
-	long lastUpdate = 0;
+	    long lastUpdate = 0;
 
 	    @Override
 	    public void handle(long now) {
-			// TODO Auto-generated method stub
-			long timeSec = (now - lastUpdate) / (1000000000 / 30); // 30 Frames every 1 sec.
-			if (timeSec >= 1) {
-			    for (int row = 0; row < grid.size(); row++) {
-					for (int col = 0; col < grid.get(0).size(); col++) {
-					    Tile tile = grid.get(row).get(col);
-					    gc.drawImage(tile.getTexture(), 50 * col, 50 * row);
-					    if (tile.getPlacedTower() != null) {
-					    	Tower tower = tile.getPlacedTower();
-					    	gc2.drawImage(tower.getImage(), 50*col-4, 50*row-15, 65, 65);
-					    }   
-					}
+		// TODO Auto-generated method stub
+		long timeSec = (now - lastUpdate) / (1000000000 / 30); // 30 Frames every 1 sec.
+		if (timeSec >= 1) {
+		    for (int row = 0; row < grid.size(); row++) {
+			for (int col = 0; col < grid.get(0).size(); col++) {
+			    Tile tile = grid.get(row).get(col);
+//			    gc.drawImage(tile.getTexture(), 50 * col, 50 * row);
+			    if (tile.getPlacedTower() != null) {
+				Tower tower = tile.getPlacedTower();
+				gc2.drawImage(tower.getImage(), 50 * col -4, 50 * row - 15, 65, 65);
 			    }
-			   
-			    towerMenuLayer.drawImage(new Image("menuTowerEmpty.png"), 0, 900, 1400, 100);
-			    valueLayer.fillText("Gold: " + money, 200, 200);
-			    lastUpdate = now;
 			}
+		    }
+
+		    towerMenuLayer.drawImage(new Image("menuTowerEmpty.png"), 0, 900, 1400, 100);
+		    String str = "Gold: " + money;
+		    gold.setText(str);
+//		    valueLayer.fillText(, 200, 200);
+		    lastUpdate = now;
+		}
 	    }
 
 	}.start();
@@ -143,73 +150,73 @@ public class TowersOfBrimstoneView extends Application {
     }
 
     private void setUpTowerMenu() {
-		int height = 90;
-		int width = 110;
-		HBox hBox = new HBox();
-		stone = new ImageButton("stone.png",width, height);
-		fire = new ImageButton("fire.png", width, height);
-		ice = new ImageButton("ice.png", width, height);
-		heavy = new ImageButton("heavy.png", width, height);
-		lightning = new ImageButton("lightning.png", width, height);
-		magic = new ImageButton("magic.png", width, height);
-		slowdown = new ImageButton("slowdown.png", width, height);
-		damageboost = new ImageButton("damageboost.png", width, height);
-	
-		hBox.setAlignment(Pos.CENTER);
-		hBox.setSpacing(100);
-		hBox.setPadding(new Insets(0, 0, 0, 50));
-		hBox.getChildren().add(stone);
-		hBox.getChildren().add(fire);
-		hBox.getChildren().add(ice);
-		hBox.getChildren().add(heavy);
-		hBox.getChildren().add(lightning);
-		hBox.getChildren().add(magic);
-		hBox.getChildren().add(slowdown);
-		hBox.getChildren().add(damageboost);
-		base.getChildren().add(hBox);
-		
-		base.setBottomAnchor(hBox, 25.0);
-		
-		for (Node btn : hBox.getChildren()) {
-			((ImageButton) btn).setOnAction(new ButtonListener());
-		}
+	int height = 90;
+	int width = 110;
+	HBox hBox = new HBox();
+	stone = new ImageButton("stone.png", width, height);
+	fire = new ImageButton("fire.png", width, height);
+	ice = new ImageButton("ice.png", width, height);
+	heavy = new ImageButton("heavy.png", width, height);
+	lightning = new ImageButton("lightning.png", width, height);
+	magic = new ImageButton("magic.png", width, height);
+	slowdown = new ImageButton("slowdown.png", width, height);
+	damageboost = new ImageButton("damageboost.png", width, height);
+
+	hBox.setAlignment(Pos.CENTER);
+	hBox.setSpacing(75);
+	hBox.setPadding(new Insets(0, 0, 0, 35));
+	hBox.getChildren().add(stone);
+	hBox.getChildren().add(fire);
+	hBox.getChildren().add(ice);
+	hBox.getChildren().add(heavy);
+	hBox.getChildren().add(lightning);
+	hBox.getChildren().add(magic);
+	hBox.getChildren().add(slowdown);
+	hBox.getChildren().add(damageboost);
+	base.getChildren().add(hBox);
+
+	base.setBottomAnchor(hBox, 25.0);
+
+	for (Node btn : hBox.getChildren()) {
+	    ((ImageButton) btn).setOnAction(new ButtonListener());
+	}
     }
 
     private class ButtonListener implements EventHandler<ActionEvent> {
 
-		/**
-		 * Handle logic when button is clicked
-		 *
-		 * @param event the ActionEvent
-		 */
-		@Override
-		public void handle(ActionEvent event) {
-			togglePlacement = true;
-			System.out.println("Tower Placement enabled: " + togglePlacement);
+	/**
+	 * Handle logic when button is clicked
+	 *
+	 * @param event the ActionEvent
+	 */
+	@Override
+	public void handle(ActionEvent event) {
+	    togglePlacement = true;
+	    System.out.println("Tower Placement enabled: " + togglePlacement);
 
-			if (event.getSource().equals(stone)) {
-				selectedTower = controller.getTowerType(1);
-				System.out.println("Selected Stone tower");
-			} else if (event.getSource().equals(fire)) {
-				selectedTower = controller.getTowerType(2);
-				System.out.println("Selected Fire tower");
-			} else if (event.getSource().equals(ice)) {
-				selectedTower = controller.getTowerType(3);
-				System.out.println("Selected Ice tower");		
-			} else if (event.getSource().equals(heavy)) {
-				selectedTower = controller.getTowerType(4);
-				System.out.println("Selected Heavy tower");		
-			} else if (event.getSource().equals(lightning)) {
-				selectedTower = controller.getTowerType(5);
-				System.out.println("Selected Lightning tower");		
-			} else if (event.getSource().equals(magic)) {
-				selectedTower = controller.getTowerType(6);
-				System.out.println("Selected Magic tower");		
-			} else if (event.getSource().equals(slowdown)) {
-				System.out.println("Unimplemented Ability 1");		
-			} else if (event.getSource().equals(damageboost)) {
-				System.out.println("Unimplemented Ability 2");		
-			}	
-		}	
+	    if (event.getSource().equals(stone)) {
+		selectedTower = controller.getTowerType(1);
+		System.out.println("Selected Stone tower");
+	    } else if (event.getSource().equals(fire)) {
+		selectedTower = controller.getTowerType(2);
+		System.out.println("Selected Fire tower");
+	    } else if (event.getSource().equals(ice)) {
+		selectedTower = controller.getTowerType(3);
+		System.out.println("Selected Ice tower");
+	    } else if (event.getSource().equals(heavy)) {
+		selectedTower = controller.getTowerType(4);
+		System.out.println("Selected Heavy tower");
+	    } else if (event.getSource().equals(lightning)) {
+		selectedTower = controller.getTowerType(5);
+		System.out.println("Selected Lightning tower");
+	    } else if (event.getSource().equals(magic)) {
+		selectedTower = controller.getTowerType(6);
+		System.out.println("Selected Magic tower");
+	    } else if (event.getSource().equals(slowdown)) {
+		System.out.println("Unimplemented Ability 1");
+	    } else if (event.getSource().equals(damageboost)) {
+		System.out.println("Unimplemented Ability 2");
+	    }
 	}
+    }
 }
