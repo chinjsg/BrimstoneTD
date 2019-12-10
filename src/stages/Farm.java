15b21d4f5297d73439aa21b6eb29687da552b468
chinjsg <dragvern@hotmail.com>
2019-12-10 15:53:54 -0700

    fixed map selection

--- src/stages/Farm.java
+++ src/stages/Farm.java
@@ -6,14 +6,13 @@ public class Farm extends MapVariant {
 
 	public Farm(int[][] map) {
 		super(map);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public ArrayList<int[]> getAllStart() {
 		ArrayList<int[]> spots = new ArrayList<int[]>();
 		int[] coord = new int[2];
-		coord[0] = 11;
+		coord[0] = 2;
 		coord[1] = 0;
 		spots.add(coord);
 		
@@ -23,10 +22,9 @@ public class Farm extends MapVariant {
 	@Override
 	public int[] getEnd() {
 		int[] coord = new int[2];
-		coord[0] = 15;
+		coord[0] = 8;
 		coord[1] = 27;
 		
 		return coord;
 	}
-
 }
--- src/stages/Oasis.java
+++ src/stages/Oasis.java
@@ -6,27 +6,25 @@ public class Oasis extends MapVariant {
 
 	public Oasis(int[][] map) {
 		super(map);
-		// TODO Auto-generated constructor stub
 	}
-
+	
 	@Override
 	public ArrayList<int[]> getAllStart() {
 		ArrayList<int[]> spots = new ArrayList<int[]>();
 		int[] coord = new int[2];
-		coord[0] = 2;
+		coord[0] = 11;
 		coord[1] = 0;
 		spots.add(coord);
 		
 		return spots;
 	}
-
+	
 	@Override
 	public int[] getEnd() {
 		int[] coord = new int[2];
-		coord[0] = 8;
+		coord[0] = 15;
 		coord[1] = 27;
 		
 		return coord;
 	}
-
 }
