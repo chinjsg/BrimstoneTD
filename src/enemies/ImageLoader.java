8821acc7086cfb317f21bcee03e174cdc2179586
chinjsg <dragvern@hotmail.com>
2019-12-11 10:59:58 -0700

    Working different firing rate

--- src/enemies/ImageLoader.java
+++ src/enemies/ImageLoader.java
@@ -32,21 +32,12 @@ public class ImageLoader {
 		loader.put("assets/ice/enemies/6/ice6.png", new Image("assets/ice/enemies/6/ice6.png"));
 		loader.put("assets/volcano/enemies/6/volcano6.png", new Image("assets/volcano/enemies/6/volcano6.png"));
 		
-<<<<<<< HEAD
 		loader.put("stoneammo.png", new Image("assets/towers/stone/stoneammo.png",20,20,false,false));
 		loader.put("magicammo.png",new Image("assets/towers/magic/magicammo.png",20,20,false,false));
 		loader.put("lightningammo.png", new Image("assets/towers/lightning/lightningammo.png",30,30,false,false));
 		loader.put("iceammo.png", new Image("assets/towers/ice/iceammo.png",20,20,false,false));
 		loader.put("heavyammo.png",new Image("assets/towers/heavy/heavyammo.png",30,20,false,false));
 		loader.put("fireammo.png", new Image("assets/towers/fire/fireammo.png",20,20,false,false));	
-=======
-		loader.put("stoneammo.png", new Image("stoneammo.png",20,20,false,false));
-		loader.put("magicammo.png",new Image("magicammo.png",20,20,false,false));
-		loader.put("lightningammo.png", new Image("lightningammo.png",30,30,false,false));
-		loader.put("iceammo.png", new Image("iceammo.png",20,20,false,false));
-		loader.put("heavyammo.png",new Image("heavyammo.png",30,20,false,false));
-		loader.put("fireammo.png", new Image("fireammo.png",20,20,false,false));
->>>>>>> refs/heads/rateOfFire
 		
 		loader.put("tower_fire.png", new Image("assets/towers/fire/tower_fire.png",65,65,false,false));	
 		loader.put("tower_heavy.png", new Image("assets/towers/heavy/tower_heavy.png",65,65,false,false));
