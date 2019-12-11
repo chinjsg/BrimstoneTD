2f6400a727fa3f01ff21180563c926dda5efc654
ABHISHEK AGARWAL <abhishekagarwal488@gmail.com>
2019-12-10 22:57:03 -0700

    Update TowerMap.java
    Added comment

--- src/towers/TowerMap.java
+++ src/towers/TowerMap.java
@@ -1,18 +1,29 @@
 package towers;
 
 import java.util.HashMap;
-
+/**
+ * @author Glen Chin
+ * @author Marko Kreso
+ * @author Abhishek Sharma
+ * @author Abhishek Agarwal
+ *
+ */
 // For storage of placed towers
 public class TowerMap {
 
 	private HashMap<Integer, Tower> placedTowers;
 	private int towerID;
-	
+	/**
+	 *  This is the constructor for the TowerMap class
+	 */
 	public TowerMap() {
 		placedTowers = new HashMap<>();
 		towerID = 1;
 	}
-	
+	/**
+	 * This will add the tower object in a HashMap.
+	 * @param tower
+	 */
 	public void addTower(Tower tower) {
 		placedTowers.put(towerID, tower);
 		towerID++;
