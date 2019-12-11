c6ba7aa574c1cd0fe7eb52ca5ed948967b6ad40f
ABHISHEK AGARWAL <abhishekagarwal488@gmail.com>
2019-12-10 18:54:11 -0700

    Update unit_testing.java

--- src/experimenting/unit_testing.java
+++ src/experimenting/unit_testing.java
@@ -2,5 +2,16 @@ package experimenting;
 
 public class unit_testing {
 	
-
+	@Test
+	public void test1() {
+//		Application.launch(TowersOfBrimstoneView.class, args);
+		
+		TowersOfBrimstoneModel model = new TowersOfBrimstoneModel();
+		TowersOfBrimstoneController controller = new TowersOfBrimstoneController(model);
+		controller.createMap(1);
+		for (int i = 0 ; i < 200000 ; i++) {
+			controller.frameUpdate(i);
+		}
+		
+	}	
 }
