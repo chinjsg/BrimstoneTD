5c8387de63548cb2f525018742fb9995b2d151f7
ABHISHEK AGARWAL <abhishekagarwal488@gmail.com>
2019-12-10 22:53:13 -0700

    Update StoneTower.java
    Adding Comments

--- src/towers/StoneTower.java
+++ src/towers/StoneTower.java
@@ -1,9 +1,19 @@
 package towers;
 
 import javafx.scene.image.Image;
-
+/**
+ * @author Glen Chin
+ * @author Marko Kreso
+ * @author Abhishek Sharma
+ * @author Abhishek Agarwal
+ *
+ */
 public class StoneTower extends Tower {
-	
+	/**
+	 * This is the constructor of the StoneTower Class.
+	 * @param row
+	 * @param col
+	 */
 	public StoneTower(int row, int col) {
 		// Tower stats
 		//  - Damage       = 15
@@ -16,7 +26,9 @@ public class StoneTower extends Tower {
 		texture = "tower_stone.png";				// 65 x 65
 		ammo = "stoneammo.png";					// 30 x 30
 	}
-	
+	/**
+	 * This will return the String of the class.
+	 */
 	public String toString() {
 		return "Stone Tower";
 	}
