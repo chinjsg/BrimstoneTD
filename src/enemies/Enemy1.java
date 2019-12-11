bd3ec94579f4c4eb03d13a593e967b14f6c3ee45
ABHISHEK AGARWAL <abhishekagarwal488@gmail.com>
2019-12-11 14:57:07 -0700

    Update Enemy1.java
    Added the comments

--- src/enemies/Enemy1.java
+++ src/enemies/Enemy1.java
@@ -5,16 +5,30 @@ import java.util.Random;
 
 import game.Tile;
 import javafx.scene.image.Image;
-
+/**
+ * This is the abstract class that represent all the enemy used in the game 
+ * @author Glen Chin
+ * @author Marko Kreso
+ * @author Abhishek Sharma
+ * @author Abhishek Agarwal
+ *
+ */
 public class Enemy1 extends Enemy{
 	private String picture;
+	/**
+	 * This is the constructor 
+	 * @param path
+	 */
 	public Enemy1(ArrayList<Tile> path) {
 		super(100, 2, 20, path,10);
 		picture = getRandomImagePath();
 
 	}
 	
-
+	/**
+	 * This is the return the String of the images which are randomly selected.
+	 * @return String
+	 */
 	private String getRandomImagePath() {
 	    Random random = new Random();
 	    String path = "";
@@ -28,10 +42,18 @@ public class Enemy1 extends Enemy{
 	    }
 	    return path;
 	}
+	/**
+	 * This will return the image path
+	 * @return String
+	 */
 	@Override
 	public String getImage() {
 		return picture;
 	}
+	/**
+	 * This will print.
+	 * @return String
+	 */
 	public String toString() {
 		return "Enemy 1";
 		
